ab6945560b53f3cb2aff0f7d1e096ba3204526f2
Şevval Tuncay <sevvaltuncy@gmail.com>
2024-07-01 00:39:17 +0300

    first

--- backend/server.js
+++ backend/server.js
@@ -5,59 +5,46 @@ const bcrypt = require("bcrypt");
 const validator = require("validator");
 const app = express();
 
-const saltRounds = 10;
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
+
   if (!email || !username || !password) {
-    return res.status(400).send("You mustt fill all fields!");
+    return res.status(400).send("You must fill all fields!");
   }
   if (!validator.isEmail(email)) {
-    return res.status(400).send("Invalid email!");
+    return res.status(400).send("Invalid email address!");
   }
   if (!validator.isStrongPassword(password)) {
-    return res.status(400).send("Not enough strong password!");
+    return res.status(400).send("Password is not strong enough!");
   }
-  bcrypt.hash(password, saltRounds, (err, hashedPassword) => {
-    if (err) {
-      console.log(err);
-      return res
-        .status(500)
-        .send("Something went wrong while your password was hashing!");
-    }
-    const checkEmailQuery = "SELECT * FROM users WHERE email = ?";
-    mysql.query(checkEmailQuery, [email], (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).send("Database error!");
-      }
-      if (results.length > 0) {
-        return res.status(400).send("This email alredy in use!");
-      }
 
-      const insertUserQuery =
-        "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
-      mysql.query(
-        insertUserQuery,
-        [username, email, hashedPassword],
-        (err, result) => {
-          if (err) {
-            console.log(err);
-            return res.status(500).send("Database error!");
-          }
-          res.send("Success registered!");
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const insertUserQuery =
+      "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+
+    mysql.query(
+      insertUserQuery,
+      [username, email, hashedPassword],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).send("Database error!");
         }
-      );
-    });
-  });
+        res.send("Registration successful!");
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error processing your request!");
+  }
 });
-//login endpoint
-app.post("/login", (req, res) => {});
 
-app.listen(4000, () => {
-  console.log(`Server running on port 4000`);
+app.listen(3000, () => {
+  console.log(`Server running on port 3000`);
 });
+
 app.get("/", (req, res) => res.status(200).json({ message: "Başarılı" }));
